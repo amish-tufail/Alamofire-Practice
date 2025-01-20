b868cfd39ce3415bd7e86be5ef0373b1f86fd58a
bisckoot <100939251+amish-tufail@users.noreply.github.com>
2025-01-20 22:25:34 +0500

    Initial Commit

---
+++ Alamofire Practice/Alamofire_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_PracticeApp.swift
+//  Alamofire Practice
+//
+//  Created by Amish Tufail on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alamofire PracticeTests/Alamofire_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_PracticeTests.swift
+//  Alamofire PracticeTests
+//
+//  Created by Amish Tufail on 20/01/2025.
+//
+
+import Testing
+@testable import Alamofire_Practice
+
+struct Alamofire_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
